cb8c39e8092177d9d5e9d43b7f565a237fb1df08
pedrok1218 <pedrorossimar92@gmail.com>
2025-10-19 19:08:47 -0300

    Delete Area rectangulo.py

--- Area rectangulo.py
+++
@@ -1,5 +0,0 @@
-print ( "este programa calcula el area de un rectangulo de 15 cm de largo y 10 de ancho" )
-Largo = 15
-Ancho = 10
-Area = Largo * Ancho
-print (f" La multiplicacion de {Largo} por {Ancho} da como resultado el area del rectangulo que es {Area}") 
